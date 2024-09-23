f368800d4f8587ab31f72a5e46e91e57c171cede
Md Roknujjaman Rony <roknujjamanrony1234@gail.com>
2024-09-23 19:06:13 +0600

    added a js file

--- app/function.js
+++ app/function.js
@@ -1,11 +1,24 @@
-let sum = 0;
+let myBalance = document.getElementById("my-balance").innerText;
+//added event listener
 document
   .getElementById("donate-now-btn")
   .addEventListener("click", function () {
     const donateMoney = getInputFieldValueById("inputfield-1");
     const accountBalance = document.getElementById("card-balance-1").innerText;
     const accountBalanceNumber = parseFloat(accountBalance);
-    const UpdateBalance1 = accountBalanceNumber + donateMoney;
-    console.log(UpdateBalance1);
-    document.getElementById("card-balance-1").innerText = UpdateBalance1;
+    if (
+      !isNaN(donateMoney) &&
+      donateMoney <= accountBalanceNumber &&
+      donateMoney > 0
+    ) {
+      const UpdateBalance1 = accountBalanceNumber + donateMoney;
+      document.getElementById("card-balance-1").innerText = UpdateBalance1;
+
+      const updatedMyBalance = parseFloat(myBalance);
+      const newBalance = updatedMyBalance - donateMoney;
+      document.getElementById("my-balance").innerText = newBalance;
+      document.getElementById("my_modal_5").showModal();
+    } else {
+      alert("Please enter a valid donation amount within your balance.");
+    }
   });
